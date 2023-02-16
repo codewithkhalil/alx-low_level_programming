625ed6f6e0b46c4ef911bb1a1f7aa1fa08bc2a8c
codewithkhalil <dareythefirst@gmail.com>
2023-02-16 12:45:17 -0800

    hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is a function that prints a sentence
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
