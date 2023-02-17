d6a93199046cdd82e2f6bd79d3107885c4ef7fd4
codewithkhalil <dareythefirst@gmail.com>
2023-02-17 14:22:58 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints an alphabet in different cases
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = 'A' ; i <= 'Z' ; i++)
+		putchar(i);
+	putchar("\n");
+	return (0);
+}
