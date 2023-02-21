50fd8e1db90ea4e3bcda73f22900730f01feace4
codewithkhalil <dareythefirst@gmail.com>
2023-02-21 15:48:03 -0800

    Signsgiy

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,17 +14,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+, ");
+		_putchar("+");
 		return (1);
 	}
 	else if (n == 0)
 	{
-		printf("0, ");
+		printf("0");
 		return (0);
 	}
 	else
 	{
-		printf("-, ");
+		printf("-");
 		return (0);
 	}
 }
