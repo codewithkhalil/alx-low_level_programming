344edfa0cd047b0bc8032af00294bb0ec677dbfa
codewithkhalil <dareythefirst@gmail.com>
2023-02-21 14:17:22 -0800

    alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,13 @@
 
 void print_alphabet_x10(void)
 {
-        int i;
-        int j;
+	int i;
+	int j;
 
-        for (i = 1 ; i <= 10 ; i++)
-        {
-                for (j = 'a' ; j <= 'z' ; j++)
-                        _putchar(j);
-                _putchar('\n');
-        }
+	for (i = 1 ; i <= 10 ; i++)
+	{
+		for (j = 'a' ; j <= 'z' ; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
 }
