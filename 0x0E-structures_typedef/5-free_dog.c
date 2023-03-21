575a5989cceb948f21b75ca5e95887575c85a660
codewithkhalil <dareythefirst@gmail.com>
2023-03-21 14:11:04 -0700

    free dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,15 +1,19 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
+
 /**
- * free_dog - frees memory allocated for a struct dog
- * @d: struct dog to free
+ * free_dog - frees a dog structure
+ * @d: pointer to struct to free
+ * Return: Nothing
  */
+
 void free_dog(dog_t *d)
 {
-	if (d)
-	{
+	if (d == NULL)
+		return;
+	if (d->name != NULL)
 		free(d->name);
+	if (d->owner != NULL)
 		free(d->owner);
-		free(d);
-	}
+	free(d);
 }
